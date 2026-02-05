c0b1b686b4204b5f3476c3d7935a013517f7b93e
Administrator <Administrator@DESKTOP-0RBNLGT>
2026-02-05 18:22:50 +0600

    refactor: transfer to DTOs from change password request

--- src/Auth.Api/Controllers/UserController.cs
+++ src/Auth.Api/Controllers/UserController.cs
@@ -183,8 +183,3 @@ public class UserController : ControllerBase
     }
 }
 
-public class ChangePasswordRequest
-{
-    public string CurrentPassword { get; set; } = string.Empty;
-    public string NewPassword { get; set; } = string.Empty;
-}
---
+++ src/Auth.Core/DTOs/ChangePasswordRequest.cs
@@ -0,0 +1,7 @@
+namespace Auth.Core.DTOs;
+
+public class ChangePasswordRequest
+{
+    public string CurrentPassword { get; set; } = string.Empty;
+    public string NewPassword { get; set; } = string.Empty;
+}
